6caf9a93acd8726c183a1064abb33657d74c816a
Lion1208 <65801114+Lion1208@users.noreply.github.com>
2024-09-30 17:13:08 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,5 +17,11 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    
+
+    printf("Desafio - Cadastro das cartas!");
+
+    
+
     return 0;
 }
